1f1217ddfb8f020a04ea4466029e932df47d29fa
Aoyukkii <ruffromane@gmail.com>
2025-10-15 15:18:46 +0200

    Add infra repositories

---
+++ Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,16 @@
+﻿using Core;
+
+namespace Infrastructure;
+
+public class CustomerRepository : ICustomerRepository
+{
+    public Task AddCustomer(CustomerPasswordDto customerPassword)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<CustomerDto>> GetAllCustomer()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Infrastructure/Repositories/SupplierRepository.cs
@@ -0,0 +1,16 @@
+﻿using Core;
+
+namespace Infrastructure;
+
+public class SupplierRepository : ISupplierRepository
+{
+    public Task AddSupplier(SupplierDto supplier)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<SupplierDto>> GetAllSupplier()
+    {
+        throw new NotImplementedException();
+    }
+}
