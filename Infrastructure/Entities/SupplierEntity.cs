caf786da92cd95d22e2b5683036882d3db7f10bd
OGR-67 <roosolivier@gmail.com>
2025-10-16 13:07:13 +0200

    Update project folder structure

--- Infrastructure/Entities/SupplierEntity.cs
+++ Infrastructure/Entities/SupplierEntity.cs
@@ -4,7 +4,7 @@ public class SupplierEntity
 {
     public int Id { get; set; }
     public string Name1 { get; set; }
-    public string Name2 { get; set; }  
+    public string Name2 { get; set; }
     public required string Email { get; set; }
     public string Address1 { get; set; }
     public string Address2 { get; set; }
